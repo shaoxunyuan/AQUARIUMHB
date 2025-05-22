7d80fcf51c3bb9b1ff733563b9ba0e360921be0c
shaoxunyuan <ysx0226@gmail.com>
2025-05-22 23:35:00 +0800

    update

--- R/MakeReferenceIsoform.R
+++ R/MakeReferenceIsoform.R
@@ -9,8 +9,7 @@
 #' @return A data frame containing the combined and processed reference isoforms
 #' 
 #' @importFrom data.table fread setnames
-#' @importFrom dplyr count
-#' @import plyr
+#' @importFrom dplyr
 #' 
 #' @export
 #' 
